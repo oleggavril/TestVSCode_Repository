9dfb274bd129ff16baae34246c570c7a8054e2f9
Oleg Gavril <oleggavril@gmail.com>
2022-08-11 22:07:35 +0300

    CommitTest

---
+++ FirstJsFile.js
@@ -0,0 +1,6 @@
+console.log("---------------------------");
+console.log("Smth to log to the console");
+console.log("---------------------------");
+ 
+var SomeString = 'A text from  a variable';
+console.log(SomeString); 
